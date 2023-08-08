77e99754f8895e39a0fa38a0dda9ecaeeb161bb7
Debmalya <debmalyasen37@gmail.com>
2023-08-08 23:21:07 +0530

    connected mongodb databse with login/sigup , but an issue having while user is submitting the data from login form , its not going to backend and not redirecting to the onboarding page

--- client/src/Components/AuthModal.js
+++ client/src/Components/AuthModal.js
@@ -1,70 +1,86 @@
-
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const AuthModal = ({setShowModal, isSignup}) => {
-
-
+const AuthModal = ({ setShowModal, isSignup }) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
 
-  console.log(email,password,confirmPassword);
+  let navigate = useNavigate();
 
+  console.log(email, password, confirmPassword);
 
-// handleClick & handleSubmit section code 
+  // handleClick & handleSubmit section code
 
-  const handleClick = ()=> {
-    setShowModal(false)
-  }
+  const handleClick = () => {
+    setShowModal(false);
+  };
 
-  const handleSubmit = (e)=> {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      if (isSignup && (password !== confirmPassword)) {
-        setError("Password need to match!")
-      } 
-      console.log('make a post request to our database')
+      if (isSignup && password !== confirmPassword) {
+        setError("Password need to match!");
+        return;
+      }
+      console.log("posting", email, password);
+      const response = await axios.post("http://localhost:8000/signup");
+
+      const success = response.status === 201;
+
+      if (success) {
+        navigate("/onboarding");
+      }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  return (<div className="auth-modal">
-    <div className="close-icon" onClick={handleClick}>⨂
+  return (
+    <div className="auth-modal">
+      <div className="close-icon" onClick={handleClick}>
+        ⨂
+      </div>
+      <h2>{isSignup ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+      <p>
+        By clicking the button below, you are agreeing to our terms and policy.{" "}
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email Address"
+          name="email"
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          name="password"
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {isSignup && (
+          <input
+            type="Password"
+            id="password-check"
+            placeholder="Confirm Password"
+            name="Password-check"
+            required={true}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
+        <input className="secondary-button" type="submit" />
+        <p>{error}</p>
+      </form>
+      <hr />
+      <h2>GET IN NOW</h2>
+      Auth Modal
     </div>
-  <h2>{isSignup ? "CREATE ACCOUNT" : "LOG IN"}</h2>
-  <p>By clicking the button below, you are agreeing to our terms and policy. </p>
-    <form onSubmit={handleSubmit}>
-
-      <input type="email"
-      id="email"
-      placeholder="Email Address"
-      name="email"
-      required={true}
-      onChange={(e) => setEmail(e.target.value)}
-       />
-      <input type="password"
-      id="password"
-      placeholder="password"
-      name="password"
-      required={true}
-      onChange={(e) => setPassword(e.target.value)}
-       />
-      { isSignup && <input type="Password"
-      id="password-check"
-      placeholder="Confirm Password"
-      name="Password-check"
-      required={true}
-      onChange={(e) => setConfirmPassword(e.target.value)}
-       />
-       }
-       <input className="secondary-button" type="submit" />
-       <p>{error}</p>
-    </form>
-    <hr />
-    <h2>GET IN NOW</h2>
-    Auth Modal
-  </div>)
+  );
 };
 export default AuthModal;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,80 @@
-const PORT = 8000;
 const express = require("express");
+const { JsonWebTokenError } = require("jsonwebtoken");
 const { MongoClient } = require("mongodb");
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+
+const PORT = 8000;
 const uri =
   "mongodb+srv://debmalyasen37:mypassword@cluster0.o6majhv.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("WELCOME TO THE BACKEND OF TRIPMATE WEB APP");
 });
-app.get("/signup", (req, res) => {
-  res.json("WELCOME TO THE BACKEND OF TRIPMATE WEB APP");
+
+app.post("/signup", async (req, res) => {
+  const client = new MongoClient(uri);
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  // unique id generation
+  const generateduserId = uuidv4();
+
+  //password hashing
+  const hashedpassword = await bcrypt.hash(password, 10);
+
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
+    const existingUser = await users.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).send("user already exists. Please Login");
+    }
+
+    const sanitizedEmail = email.toLowerCase();
+
+    const data = {
+      user_id: generateduserId,
+      email: sanitizedEmail,
+      hashed_password: hashedpassword,
+    };
+
+    const insertedUser = await users.insertOne(data);
+
+    const token = jwt.sign(insertedUser, sanitizedEmail, {
+      expiresIn: 60 * 24,
+    });
+    res
+      .status(201)
+      .json({ token, userId: generateduserId, email: sanitizedEmail });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal server error" });
+  } finally {
+    await client.close();
+  }
 });
 
-app.listen(PORT, () => console.log("server running on PORT " + PORT));
+app.get("/users", async (req, res) => {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
+    const returnedUsers = await users.find().toArray();
+    res.send(returnedUsers);
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(PORT, () => console.log("Server running on PORT " + PORT));
